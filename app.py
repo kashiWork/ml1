e0c635654f048f29669748c33811630721ed804d
kashiWork <kashinath18@proton.me>
2025-03-10 14:11:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 from fastapi import FastAPI
-import joblib  # For loading the trained model
+import joblib  
 import numpy as np
 from pydantic import BaseModel
 
-# Load your trained model (ensure it's uploaded in the same directory as this script)
-model = joblib.load("model.pkl")  # Change filename if needed
+model = joblib.load("model.pkl")  
 
-# Initialize FastAPI app
+#  FastAPI app
 app = FastAPI()
 
 # Define input schema using Pydantic
@@ -17,30 +16,26 @@ class ModelInput(BaseModel):
     Category_Electronics: int
     Category_Fashion: int
     Category_Home: int
-    Level_high: int  # Always 1
+    Level_high: int  
     Level_low: int
     Level_normal: int
 
 # MinMax normalization for price
-PRICE_MIN = 2300  # Set actual min price from training data
-PRICE_MAX = 150000  # Set actual max price from training data
+PRICE_MIN = 2300  
+PRICE_MAX = 150000  
 
 def normalize_price(price):
     return (price - PRICE_MIN) / (PRICE_MAX - PRICE_MIN)
 
 @app.post("/predict")
 def predict(data: ModelInput):
-    # Preprocess input
     normalized_price = normalize_price(data.Price)
-    
-    # Create feature array
+    #  feature array
     input_data = np.array([
         normalized_price,data.Category_Beauty, data.Category_Books,
         data.Category_Electronics, data.Category_Fashion, data.Category_Home,
         data.Level_high, data.Level_low, data.Level_normal
     ]).reshape(1, -1)
     
-    # Get prediction
     prediction = model.predict(input_data)
-    
     return {"predicted_month": int(prediction[0])}
